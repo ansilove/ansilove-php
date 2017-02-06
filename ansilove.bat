a1c3cc33ad02ed678bfb03e237066d88bfe7bd9e
Frederic Cambus <fred@statdns.com>
2017-02-06 10:14:24 +0100

    Bump copyright year

--- ansilove.bat
+++ ansilove.bat
@@ -6,11 +6,11 @@ rem /* __\   \./  \| \. \  |_| \|  |_/    \  |  /  _/                        */
 rem /* \___\__|__|\___|_____/___|____/____/\___/\____/plur                   */
 rem /*                                                                       */
 rem /* Ansilove/PHP 1.11                                                     */
-rem /* Copyright (c) 2003-2016, Frederic Cambus                              */
+rem /* Copyright (c) 2003-2017, Frederic Cambus                              */
 rem /* https://www.ansilove.org                                              */
 rem /*                                                                       */
 rem /* Created:      2003-07-17                                              */
-rem /* Last Updated: 2016-07-19                                              */
+rem /* Last Updated: 2017-02-06                                              */
 rem /*                                                                       */
 rem /* Ansilove is released under the BSD 2-Clause license.                  */
 rem /* See LICENSE file for details.                                         */
